5ade6d82ad98c6c27ffc00d53f03c9acc8811c9f
brunorw09 <brwrocha7@gmail.com>
2025-11-03 19:47:53 -0300

    Add files via upload

---
+++ Atividade 8/Pmatrizes/Pmatrizes/Exercicio5.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Pmatrizes
+{
+    partial class Exercicio5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblRespostas = new System.Windows.Forms.Label();
+            this.lstRespostas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(74, 263);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(132, 47);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(74, 358);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(132, 45);
+            this.btnFechar.TabIndex = 1;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lblRespostas
+            // 
+            this.lblRespostas.AutoSize = true;
+            this.lblRespostas.Location = new System.Drawing.Point(288, 67);
+            this.lblRespostas.Name = "lblRespostas";
+            this.lblRespostas.Size = new System.Drawing.Size(86, 20);
+            this.lblRespostas.TabIndex = 2;
+            this.lblRespostas.Text = "Respostas";
+            // 
+            // lstRespostas
+            // 
+            this.lstRespostas.FormattingEnabled = true;
+            this.lstRespostas.ItemHeight = 20;
+            this.lstRespostas.Location = new System.Drawing.Point(292, 90);
+            this.lstRespostas.Name = "lstRespostas";
+            this.lstRespostas.Size = new System.Drawing.Size(401, 324);
+            this.lstRespostas.TabIndex = 3;
+            // 
+            // Exercicio5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 434);
+            this.Controls.Add(this.lstRespostas);
+            this.Controls.Add(this.lblRespostas);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Exercicio5";
+            this.Text = "Exercicio5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label lblRespostas;
+        private System.Windows.Forms.ListBox lstRespostas;
+    }
+}
---
+++ Atividade 8/Pmatrizes/Pmatrizes/Exercicio5.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pmatrizes
+{
+    public partial class Exercicio5 : Form
+    {
+        public Exercicio5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string[] gabarito = { "A", "B", "C", "D", "E", "B", "C", "A", "D", "E" };
+            String[,] alunoMatriz = new string[10, 10];
+            string[] opcoesValidas = { "A", "B", "C", "D", "E" };
+            int i, j;
+            string finalNotas = "";
+
+
+            // alunos
+            for (i = 0; i < 10; i++)
+            {
+                // respostas
+                for (j = 0; j < 10; j++)
+                {
+                    string resposta = "";
+                    resposta = Interaction.InputBox($"Insira a resposta da questão {j + 1}", $"Aluno {i + 1}").ToUpper();
+
+                    if (!opcoesValidas.Contains(resposta))
+                    {
+                        MessageBox.Show("Opção inválida, escolha entre A, B, C, D ou E");
+                        j--;
+                    }
+                    else
+                    {
+                        alunoMatriz[i, j] = resposta;
+                    }
+
+                }
+            }
+
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    if (alunoMatriz[i, j] == gabarito[j])
+                        finalNotas += $"O Aluno {i + 1} acertou a questão {j + 1}: era {gabarito[j]} escolheu {alunoMatriz[i, j]}\n";
+                    else
+                        finalNotas += $"O Aluno {i + 1} errou a questão {j + 1}: era {gabarito[j]} escolheu {alunoMatriz[i, j]}\n";
+                }
+            }
+
+            lstRespostas.Items.AddRange(finalNotas.Split('\n'));
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+    
+}
